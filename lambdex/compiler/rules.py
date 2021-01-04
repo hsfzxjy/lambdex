fa9be06e7e67a6b2119bff131433669c15b260d2
hsfzxjy <hsfzxjy@gmail.com>
2021-01-04 23:52:48 +0800

    Split out `_compile_stmts`

--- lambdex/compiler/rules.py
+++ lambdex/compiler/rules.py
@@ -10,6 +10,18 @@ __all__ = ['Rules']
 Rules = FunctionRegistry('Rules')
 
 
+def _compile_stmts(ctx: Context, stmts):
+    compiled_statements = []
+    for statement in stmts:
+        compiled_statement = ctx.compile(statement)
+        if isinstance(compiled_statement, ast.expr):
+            compiled_statement = ast.Expr(compiled_statement)
+
+        compiled_statements.append(compiled_statement)
+
+    return compiled_statements
+
+
 @Rules.register(ast.FunctionDef)
 def r_def(node: ast.Call, ctx: Context):
     check(node.args[0], ast.Lambda)
@@ -18,13 +30,7 @@ def r_def(node: ast.Call, ctx: Context):
     check(lambda_.body, ast.List)
     statements: ast.List = lambda_.body
 
-    compiled_statements = []
-    for statement in statements.elts:
-        compiled_statement = ctx.compile(statement)
-        if isinstance(compiled_statement, ast.expr):
-            compiled_statement = ast.Expr(compiled_statement)
-
-        compiled_statements.append(compiled_statement)
+    compiled_statements = _compile_stmts(ctx, statements.elts)
 
     new_function = ast.FunctionDef(
         name=ctx.select_name_and_use('anonymous'),
