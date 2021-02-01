e00ac5d7a05f4b5c25ff44e31ce98dd368b247b3
hsfzxjy <hsfzxjy@gmail.com>
2021-02-01 13:30:46 +0800

    Add streaming API for `FileResource`

--- lambdex/fmt/utils/io.py
+++ lambdex/fmt/utils/io.py
@@ -5,6 +5,7 @@ from ..config import Config
 from .result import Result
 
 import difflib
+from io import BytesIO
 
 
 class FileResource:
@@ -17,19 +18,25 @@ class FileResource:
         self.config = config
         self.result = result
         self.filename = filename
-        self._original_content = None
+        self._buffer = BytesIO()
 
-    def read(self) -> str:
-        if self.config.from_stdin:
-            return self.result.output
+    def read_text(self) -> str:
+        return self.open().read().decode()
 
-        self._original_content = pathlib.Path(self.filename).read_text()
-        return self._original_content
+    def open(self):
+        if self.result.output is not None:
+            self._buffer = BytesIO(self.result.output)
+        else:
+            with pathlib.Path(self.filename).open('rb') as fd:
+                self._buffer = BytesIO(fd.read())
+
+        return self._buffer
 
     def write(self, output: str):
         content = output
         if self.config.print_diff:
-            before = self._original_content.splitlines()
+            self._buffer.seek(0)
+            before = self._buffer.read().decode().splitlines()
             after = output.splitlines()
             content = '\n'.join(
                 difflib.unified_diff(
