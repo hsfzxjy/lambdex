bfad8d2dc57bad2ca4f86b579dfcb0ab546c6bf0
hsfzxjy <hsfzxjy@gmail.com>
2021-02-04 13:39:34 +0800

    Rework `lambdex.fmt.core.api`

--- lambdex/fmt/core/api.py
+++ lambdex/fmt/core/api.py
@@ -1,23 +1,10 @@
-from typing import Optional
-
-from ..config import Config
-from ..utils.result import Result
 from .tkutils.tokenize import tokenize
 from .transforms import transform, AsCode
-from ..utils.io import FileResource
 
 
-def format_files(config: Config, result: Result):
-    filenames = config.files or [None]
-    for filename in filenames:
-        format_file(config, result, filename)
+def FormatCode(source):
 
-
-def format_file(config: Config, result: Result, filename: Optional[str] = None):
-    file_resource = FileResource(config, result, filename)
-    fd = file_resource.open()
-
-    seq = tokenize(fd.__next__)
+    seq = tokenize(source)
     output = AsCode(transform(seq))
 
-    file_resource.write(output)
+    return output
