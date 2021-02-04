f099941cd0a7e05975bfb742a8137c9ff3f95754
hsfzxjy <hsfzxjy@gmail.com>
2021-02-04 14:16:32 +0800

    Fix bug: `sys.path` should not include CWD when run as program

--- lambdex/__init__.py
+++ lambdex/__init__.py
@@ -1,3 +1,18 @@
-from .keywords import def_
+import os
+import sys
+import inspect
 
-__all__ = ['def_']
+for f in inspect.stack()[1:]:
+    if not f.filename.startswith('<'):
+        break
+
+if f.filename.startswith(os.path.dirname(os.__file__)):
+    if sys.path and sys.path[0] == os.getcwd():
+        sys.path = sys.path[1:]
+else:
+    __all__ = ['def_']
+    from .keywords import def_
+
+del os, sys, inspect, f
+
+__version__ = "0.0.1"
