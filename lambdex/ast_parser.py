4c5ff83ccafd4b47672a049cb7c67cdd7a55ffc3
hsfzxjy <hsfzxjy@gmail.com>
2021-02-04 16:15:05 +0800

    Remove dependency to `astcheck` and `astsearch`

--- lambdex/ast_parser.py
+++ lambdex/ast_parser.py
@@ -1,16 +1,14 @@
 import ast
 import typing
 import inspect
-import astcheck
-import astsearch
 
 from lambdex.utils.ast import ast_from_source
 
 __all__ = ['lambda_to_ast']
 
 
-def _shallow_match_ast(node, pattern):
-    if astcheck.is_ast_like(node, pattern):
+def _shallow_match_ast(node, is_matched_fn):
+    if is_matched_fn(node):
         yield node
         return
 
@@ -18,16 +16,35 @@ def _shallow_match_ast(node, pattern):
         if isinstance(value, list):
             for item in value:
                 if isinstance(item, ast.AST):
-                    yield from _shallow_match_ast(item, pattern)
+                    yield from _shallow_match_ast(item, is_matched_fn)
         elif isinstance(value, ast.AST):
-            yield from _shallow_match_ast(value, pattern)
+            yield from _shallow_match_ast(value, is_matched_fn)
+
+
+def _make_pattern(keyword: str, identifier: str):
+    def _pattern(node: ast.AST) -> bool:
+        if node.__class__ is not ast.Call:
+            return False
+
+        declarer = node.func
+        if not identifier:
+            return declarer.__class__ is ast.Name and keyword == declarer.id
+        else:
+            return (
+                declarer.__class__ is ast.Attribute \
+                and declarer.value.__class__ is ast.Name \
+                and keyword == declarer.value.id \
+                and identifier == declarer.attr
+            )
+
+    return _pattern
 
 
 def lambda_to_ast(lambda_object: typing.Callable, *, keyword: str, identifier: str = ''):
     tree = ast_from_source(lambda_object, keyword)
     if isinstance(tree, ast.Expr) and isinstance(tree.value, ast.Lambda):
         return tree.value
-    pattern = astsearch.prepare_pattern('{}{}(?)'.format(keyword, '.' + identifier if identifier else ''))
+    pattern = _make_pattern(keyword, identifier)
     matched = list(_shallow_match_ast(tree, pattern))
 
     if not len(matched):
