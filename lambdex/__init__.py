8a43346c087db4f6eb1bc158e6a5554dfce640a1
hsfzxjy <hsfzxjy@gmail.com>
2021-02-07 12:45:16 +0800

    Fix bug: use `sys` instead of `inspect` to obtain stack
    `sys` has less dependencies than `inspect` and is trouble-less

--- lambdex/__init__.py
+++ lambdex/__init__.py
@@ -1,18 +1,17 @@
 import os
 import sys
-import inspect
 
-for f in inspect.stack()[1:]:
-    if not f.filename.startswith('<'):
-        break
+f = sys._getframe(1)
+while f.f_code is not None and f.f_code.co_filename.startswith('<'):
+    f = f.f_back
 
-if f.filename.startswith(os.path.dirname(os.__file__)):
+if f and f.f_code and f.f_code.co_filename.startswith(os.path.dirname(os.__file__)):
     if sys.path and sys.path[0] == os.getcwd():
         sys.path = sys.path[1:]
 else:
     __all__ = ['def_']
     from .keywords import def_
 
-del os, sys, inspect, f
+del os, sys, f
 
 __version__ = "0.1.0"
